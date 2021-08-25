471c12ad808f63d782791c8dd012da7f89ee4497
github lion-king-IT <reo.runningg@gmail.com>
2021-08-25 19:29:05 +0900

    add adapter in recyclerview

--- app/src/main/java/com/reo/running/samplegroupie/MainActivity.kt
+++ app/src/main/java/com/reo/running/samplegroupie/MainActivity.kt
@@ -2,12 +2,17 @@ package com.reo.running.samplegroupie
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.reo.running.samplegroupie.databinding.ActivityMainBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        val bindig = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindig.root)
 
+        val groupAdapter = GroupAdapter<ViewHolder>()
+        bindig.recyclerView.adapter = groupAdapter
     }
 }
