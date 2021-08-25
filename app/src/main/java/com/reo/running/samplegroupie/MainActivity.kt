64e7317124a763839855f2466daa9f1a624b10f2
github lion-king-IT <reo.runningg@gmail.com>
2021-08-25 19:06:55 +0900

    delete cached

--- app/src/main/java/com/reo/running/samplegroupie/MainActivity.kt
+++ app/src/main/java/com/reo/running/samplegroupie/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.reo.running.samplegroupie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+
     }
 }
